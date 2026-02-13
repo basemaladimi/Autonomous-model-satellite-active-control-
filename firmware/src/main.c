585bbded510f9e330b4fd8ab6e2260c6ddb083b7
Basem <basemaladimi0@gmail.com>
2026-02-13 02:33:50 +0000

    Basic measurement of Pitch and Roll.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,6 +1,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
+#include <math.h>
 
 float A_inv[3][3] = {
     {1.011020f, -0.007368f, 0.008895f}, 
@@ -8,7 +9,10 @@ float A_inv[3][3] = {
     {0.008895f, 0.002979f, 1.000076f}
 };
 float b_accel[3] = {0.145774f, -0.047434f, -0.799729f};
-
+float pitch = 0.0f;
+float roll = 0.0f;
+float rad_to_deg = 57.29577951f;
+ 
 void trigger_handler(const struct device *dev, const struct sensor_trigger *trig) {
     struct sensor_value accel[3];
     float raw_a[3], cal_a[3], shifted_a[3];
@@ -24,11 +28,15 @@ void trigger_handler(const struct device *dev, const struct sensor_trigger *trig
     for (int i = 0; i < 3; i++) {
         cal_a[i] = A_inv[i][0] * shifted_a[0] + A_inv[i][1] * shifted_a[1] + A_inv[i][2] * shifted_a[2];
     }
+    roll = atan2f(cal_a[0], cal_a[2])*rad_to_deg;
+    pitch = atan2f(cal_a[1], cal_a[2])*rad_to_deg;
 
-    printk("%f,%f,%f\n", 
-           (double)(cal_a[0]), 
-           (double)(cal_a[1]), 
-           (double)(cal_a[2]));
+    printk("%f,%f,%f,%f,%f\n", 
+            (double)(cal_a[0]), 
+            (double)(cal_a[1]), 
+            (double)(cal_a[2]),
+            (double)roll,
+            (double)pitch);
 }
 
 int main(void) {
