09f053fea4fb35246ca90b0c173bdc5bf910fc6c
Basem <basemaladimi0@gmail.com>
2026-03-28 06:21:45 +0000

    The latest updates were done to the MPU sensor readings before adding the MS5611 pressure sensor

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,6 +3,8 @@
 #include <zephyr/drivers/sensor.h>
 #include <math.h>
 #include <time.h>
+#include <zephyr/drivers/i2c.h>
+
 
 float A_inv[3][3] = {
     {1.011020f, -0.007368f, 0.008895f}, 
@@ -17,8 +19,8 @@ int64_t last_time = 0;
 float dt = 0.0f;
 float rollg = 0.0f, pitchg = 0.0f, yawg = 0.0f;
 float rollc = 0.0f, pitchc = 0.0f;
-float cnfdne_wght_rol = 0.009f;
-float cnfdne_wght_pitch = 0.01f;
+float cnfdne_wght_rol = 0.004f;
+float cnfdne_wght_pitch = 0.004f;
 float level_offsets[2] = {0.0f, 0.0f};
 float gx_cal = 0.0f, gy_cal = 0.0f, gz_cal = 0.0f;
 
@@ -50,7 +52,6 @@ void trigger_handler(const struct device *dev, const struct sensor_trigger *trig
         cal_a[i] = A_inv[i][0] * shifted_a[0] + A_inv[i][1] * shifted_a[1] + A_inv[i][2] * shifted_a[2];
     }
 
-
     rollg = rollg + (-(raw_g[1] - gy_cal))* dt;
     pitchg = pitchg + (raw_g[0] - gx_cal)* dt;
     yawg = yawg + raw_g[2] * dt;
@@ -60,40 +61,83 @@ void trigger_handler(const struct device *dev, const struct sensor_trigger *trig
 
     rollc = (rolla * cnfdne_wght_rol + (1.0f - cnfdne_wght_rol) * (rollc + (-(raw_g[1] - gy_cal))* dt));
     pitchc = (pitcha * cnfdne_wght_pitch + (1.0f - cnfdne_wght_pitch) * (pitchc + (raw_g[0] - gx_cal)* dt));
-
     printk("rolla:%f, pitcha:%f, rollc:%f, pitchc:%f\n",
-            (double)(rolla), 
-            (double)(pitcha), 
-            (double)(rollc),
-            (double)(pitchc));
-}
+        (double)(rolla),
+        (double)(pitcha),
+        (double)(rollc),
+        (double)(pitchc));
+
+}   
 
 int main(void) {
     const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(my_mpu));
+    const struct device *dev_baro = DEVICE_DT_GET(DT_NODELABEL(my_ms));
     struct sensor_value a_tmp[3], g_tmp[3];
     float a_sum[3] = {0.0f, 0.0f, 0.0f}, g_sum[3] = {0.0f, 0.0f, 0.0f};
+    float a_baro = 0.0f;
     int s = 200;
+    struct sensor_value pressure, temperature;
+    float AltitudeBarometer;
 
     k_msleep(500);
-    if (!device_is_ready(dev)) return 0;
+
+    if (!device_is_ready(dev)){
+        printk("MPU6050 not ready!\n");
+        return 0;
+    }
+    if (!device_is_ready(dev_baro)){
+        printk("Barometer not ready!\n");
+        return 0;
+    }
+
+    // 1. Get the I2C bus device that the MPU6050 is connected to
+    const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c0));
+    if (!device_is_ready(i2c_dev)) {
+        printk("I2C bus not ready!\n");
+        return 0;
+    }
+    
+    // 2. Set DLPF to Level 1 (Sets base clock to 1000 Hz and filters noise)
+    i2c_reg_write_byte(i2c_dev, 0x68, 0x1A, 0x01);
+
+    // 3. Set Sample Rate Divider to 4 (1000 Hz / (1 + 4) = 200 Hz)
+    i2c_reg_write_byte(i2c_dev, 0x68, 0x19, 0x04);
+    
+    for (int i = 0; i < 50; i++){
+        sensor_sample_fetch(dev);
+        k_msleep(3);
+    }
+    for (int i = 0; i < 50; i++){
+        sensor_sample_fetch(dev_baro);
+        k_msleep(10);
+    }
+
     for (int i = 0; i < s; i++) {
         sensor_sample_fetch(dev);
         sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, a_tmp);
         sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, g_tmp);
-        a_sum[0] += (float)sensor_value_to_double(&a_tmp[0]);
-        a_sum[1] += (float)sensor_value_to_double(&a_tmp[1]);
-        a_sum[2] += (float)sensor_value_to_double(&a_tmp[2]);
-        g_sum[0] += (float)sensor_value_to_double(&g_tmp[0]);
-        g_sum[1] += (float)sensor_value_to_double(&g_tmp[1]);
-        g_sum[2] += (float)sensor_value_to_double(&g_tmp[2]);
+        for (int j = 0; j < 3; j++) {
+            a_sum[j] += (float)sensor_value_to_double(&a_tmp[j]);
+            g_sum[j] += (float)sensor_value_to_double(&g_tmp[j]);
+        }
         k_msleep(3);
     }
+    
+    for (int i = 0; i < s; i++) {
+        sensor_sample_fetch(dev_baro);
+        sensor_channel_get(dev_baro, SENSOR_CHAN_PRESS, &pressure);
+        a_baro += (float)sensor_value_to_double(&pressure);
+        k_msleep(15);
+    }
+
     float ax = (a_sum[0] / s) - b_accel[0], ay = (a_sum[1] / s) - b_accel[1], az = (a_sum[2] / s) - b_accel[2];
     gx_cal = (g_sum[0] / s) * rad_to_deg, gy_cal = (g_sum[1] / s) * rad_to_deg, gz_cal = (g_sum[2] / s) * rad_to_deg;
     level_offsets[0] = A_inv[0][0] * ax + A_inv[0][1] * ay + A_inv[0][2] * az;
     level_offsets[1] = A_inv[1][0] * ax + A_inv[1][1] * ay + A_inv[1][2] * az;
     struct sensor_trigger trig = {.type = SENSOR_TRIG_DATA_READY, .chan = SENSOR_CHAN_ALL};
     sensor_trigger_set(dev, &trig, trigger_handler);
+    a_baro = a_baro/s;
+
 
     while (1) k_sleep(K_FOREVER);
 }
